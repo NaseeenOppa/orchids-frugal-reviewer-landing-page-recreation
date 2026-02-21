893c6ed95b5ea6445a800907e04728b4ef14ef23
NaseeenOppa <157215529+NaseeenOppa@users.noreply.github.com>
2026-02-22 00:13:24 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import VisualEditsMessenger from "../visual-edits/VisualEditsMessenger";
 import ErrorReporter from "@/components/ErrorReporter";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Frugal Reviewer",
